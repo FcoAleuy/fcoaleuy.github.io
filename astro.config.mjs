2572e72d2356fe33e3acb5753db4f60ad4056b50
Francisco Aleuy <faleuy@gmail.com>
2026-01-16 15:33:46 -0800

    Subiendo cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://faleuy.github.io',
-  base: '/',
+  site: 'https://fcoaleuy.github.io',
+  base: '/personal-site/', 
   integrations: [mdx()],
 });
