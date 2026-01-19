6dcc08862ed19827714954b9b20e546c7f2d2bbe
Francisco Aleuy <faleuy@gmail.com>
2026-01-19 10:02:48 -0800

    Fix domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://fcoaleuy.github.io',
-  base: '/personal-site/', 
+  // Tu dominio real (importante para el SEO y sitemap)
+  site: 'https://franciscoaleuy.ca',
+  
+  // La base debe ser la raíz, porque ya no usas la URL larga de GitHub
+  base: '/', 
+  
   integrations: [mdx(), react()],
 });
